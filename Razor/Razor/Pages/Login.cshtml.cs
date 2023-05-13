a3053f2a368b0f5af2b4c86d8258f37806503dbf
cengiz <cengiz.ergun.0101@gmail.com>
2023-05-13 21:09:11 +0300

    signup and login layout

--- Razor/Razor/Pages/Login.cshtml.cs
+++ Razor/Razor/Pages/Login.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Razor.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            return Page();
+        }
+
+        [BindProperty]
+        public LoginViewModel loginViewModel { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //call api appplication
+
+            return RedirectToPage("./Index");
         }
     }
+
+    public class LoginViewModel
+    {
+
+        [Required, StringLength(50), EmailAddress]
+        public string Email { get; set; }
+        [Required, StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
 }
+
